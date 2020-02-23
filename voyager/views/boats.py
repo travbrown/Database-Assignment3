e44a0a5e41ba2b2a2377401fede348c12749d77f
Travis Brown <travjbrown98@gmail.com>
2020-02-23 14:26:31 -0800

    set up infrastructure for all links under Views, using a false filler sql command. need to enter the correct sql commands

--- voyager/views/boats.py
+++ voyager/views/boats.py
@@ -6,8 +6,23 @@ from flask import escape
 
 from voyager.db import get_db, execute
 
+
 def boats(conn):
-    return execute(conn, "SELECT b.bid, b.name, b.color FROM Boats AS b")
+    sqlCommand = "SELECT b.bid, b.name, b.color FROM Boats AS b"
+    return execute(conn, sqlCommand)
+
+
+def boats_by_popularity(conn):
+    # TODO: Enter the correct SQL Command
+    sqlCommand = "select * from boats"
+    return execute(conn, sqlCommand)
+
+
+def boats_sailed_by(conn):
+    # TODO: Enter the correct SQL Command
+    sqlCommand = "select * from boats"
+    return execute(conn, sqlCommand)
+
 
 def views(bp):
     @bp.route("/boats")
@@ -15,3 +30,18 @@ def views(bp):
         with get_db() as conn:
             rows = boats(conn)
         return render_template("table.html", name="boats", rows=rows)
+
+    @bp.route("/boats/by-popularity")
+    def _boats_by_popularity():
+        with get_db() as conn:
+            rows = boats_by_popularity(conn)
+        return render_template("table.html", name="Boats by Popularity", rows=rows)
+
+    @bp.route("/boats/sailed-by")
+    def _boats_sailed_by():
+        with get_db() as conn:
+            rows = boats_sailed_by(conn)
+        return render_template(
+            "table.html",
+            name="Boats Sailed By",
+            rows=rows)
--- voyager/views/sailors.py
+++ voyager/views/sailors.py
@@ -9,12 +9,57 @@ from voyager.db import get_db, execute
 from voyager.validate import validate_field, render_errors
 from voyager.validate import NAME_RE, INT_RE, DATE_RE
 
+
 def sailors(conn):
     return execute(conn, "SELECT s.sid, s.name,s.age,s.experience FROM Sailors AS s")
 
+
+def sailors_who_sailed(conn):
+    # TODO: Enter the correct SQL Command
+    sqlCommand = "select * from sailors"
+    return execute(conn, sqlCommand)
+
+
+def sailors_on_date(conn):
+    # TODO: Enter the correct SQL Command
+    sqlCommand = "select * from sailors"
+    return execute(conn, sqlCommand)
+
+
+def sailors_who_sailed_certain_color_boat(conn):
+    # TODO: Enter the correct SQL Command
+    sqlCommand = "select * from sailors"
+    return execute(conn, sqlCommand)
+
+
+def sailors_add(conn):
+    # TODO: Enter the correct SQL Command
+    # TODO: Implement operations & views
+    sqlCommand = "select * from sailors"
+    return execute(conn, sqlCommand)
+
+
 def views(bp):
-	@bp.route("/sailors")
-	def _sailors():
-		with get_db() as conn:
-			rows = sailors(conn)
-		return render_template("table.html", name="Sailors", rows=rows)
+    @bp.route("/sailors")
+    def _sailors():
+        with get_db() as conn:
+            rows = sailors(conn)
+        return render_template("table.html", name="Sailors", rows=rows)
+
+    @bp.route("/sailors/who-sailed")
+    def _sailors_who_sailed():
+        with get_db() as conn:
+            rows = sailors_who_sailed(conn)
+        return render_template("table.html", name="Sailors who sailed", rows=rows)
+
+    @bp.route("/sailors/who-sailed-on-date")
+    def _sailors_on_date():
+        with get_db() as conn:
+            rows = sailors_on_date(conn)
+        return render_template("table.html", name="Sailors who sailed on date", rows=rows)
+
+    @bp.route("/sailors/who-sailed-on-boat-of-color")
+    def _sailors_who_sailed_certain_color_boat():
+        with get_db() as conn:
+            rows = sailors_who_sailed_certain_color_boat(conn)
+        return render_template("table.html", name="Sailors who sailed on boat of a certain color", rows=rows)
